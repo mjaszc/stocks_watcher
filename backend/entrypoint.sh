9effcb0821873d9852fa170e5670a8c96c45f576
mjaszc <jaszczaninmarceli@gmail.com>
2025-11-26 19:07:56 +0100

    added celery and rabbitmq to docker compose config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,5 @@ set -e
 # Run migrations
 alembic upgrade head 
 
-uv run -m data.load_stock_data
-
-uv run -m uvicorn main:app --port 8000
+# uv run -m uvicorn main:app
+fastapi run --workers 4 main.py
