fac59265987ac133aff3bb020e907d825e0d213f
Marceli <jaszczaninmarceli@gmail.com>
2025-12-11 18:36:54 +0100

    continue ansible config and storing secrets (#3)
    * added nginx and ufw base ansible config
    
    * added celery and rabbitmq to docker compose config
    
    * added nginx to container setup
    
    * added nginx as separate container in compsoe
    
    * added certbot
    
    * fixed naming in ufw and deleted nginx
    
    * fixed nginx and container connection bugs, deleted certbot config for a moment
    
    * ansible role changes need to fix after setting production compose
    
    * Added dev/prod compose, encrypted secrets where needed
    
    * fixes to docker config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # Run migrations
 alembic upgrade head 
+echo "Migrations complete. Starting application server..."
 
-# uv run -m uvicorn main:app
-fastapi run --port 8000 --workers 4 main.py
+exec fastapi run --port 8000 --workers 4 main.py
