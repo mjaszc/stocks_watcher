270959d16ce0ef0decade1104e7becd2eb90ccb0
mjaszc <jaszczaninmarceli@gmail.com>
2025-12-14 23:30:56 +0100

    changes to prod docker compose

--- frontend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-envsubst '${NGINX_SERVER_NAME}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
-
-mv /tmp/default.conf /etc/nginx/conf.d/default.conf
-
-# execute the CMD from the Dockerfile (nginx -g daemon off;)
-exec "$@"
